6ca701e7486b5755fd088ebf872c9f04cd087952
shobhitjawa <39236815+shobhitjawa@users.noreply.github.com>
2019-11-06 17:47:44 +0530

    Add files via upload

---
+++ lengthofloopinll1.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+  
+/* Link list node */
+struct Node  
+{  
+    int data;  
+    struct Node* next;  
+};  
+  
+// Returns count of nodes present in loop.  
+int countNodes(struct Node *n)  
+{  
+  int count=1;
+  struct Node* temp=n;
+  while(temp->next!=n)
+  {
+  	count++;
+  	temp=temp->next;
+  }
+  return count;
+}  
+  
+int countNodesinLoop(struct Node *list)  
+{  
+    struct Node *slow_p = list, *fast_p = list;  
+  
+    while (slow_p && fast_p &&  
+                     fast_p->next)  
+    {  
+        slow_p = slow_p->next;  
+        fast_p = fast_p->next->next;  
+        if (slow_p == fast_p)  
+            return countNodes(slow_p);  
+    } 
+    return 0;  
+}  
+  
+struct Node *newNode(int key)  
+{  
+    struct Node *temp =  
+   (struct Node*)malloc(sizeof(struct Node));  
+    temp->data = key;  
+    temp->next = NULL;  
+    return temp;  
+}  
+int main()  
+{  
+    struct Node *head = newNode(1);  
+    head->next = newNode(2);  
+    head->next->next = newNode(3);  
+    head->next->next->next = newNode(4);  
+    head->next->next->next->next = newNode(5);  
+    head->next->next->next->next->next = head->next;  
+  
+    cout << countNodesinLoop(head) << endl;  
+  
+    return 0;  
+}  
+  
