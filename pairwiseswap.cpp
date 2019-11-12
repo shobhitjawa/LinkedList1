070e97f2003ec9340ab0195c66e598e7b27a6be6
shobhitjawa <39236815+shobhitjawa@users.noreply.github.com>
2019-11-12 18:06:43 +0530

    Update pairwiseswap.cpp

--- pairwiseswap.cpp
+++ pairwiseswap.cpp
@@ -1,81 +1,58 @@
-#include<stdio.h>
-#include<iostream>
-using namespace std;
-struct node{
-	struct  node* next;
-	int data;
-};
-node* createnode(int data){                 //this is wrong code not executing...
-	
-  struct node* p;
-  p=new (struct node);
-  p->data=data;
-  p->next=NULL;
-  return p;
-}
-void insert(struct node** head,int r)
+#include <bits/stdc++.h> 
+using namespace std; 
+
+class Node { 
+public: 
+    int data; 
+    Node* next; 
+}; 
+void pairWiseSwap(Node* head) 
+{ 
+    Node* temp = head; 
+    if(temp==NULL)
+    return;
+    while (temp!= NULL && temp->next != NULL) { 
+    
+        swap(temp->data, 
+             temp->next->data);
+			 temp=temp->next->next; 
+        //temp1 = temp->next; 
+    } 
+} 
+void push(Node** head_ref, int new_data)
 {
-		struct node *p,*y;
-	p=createnode(r);
-	if(*head==NULL)
-	{
-		*head=p;
-		p->next=NULL;
-	}                                                     
-	else                                               
-	{
-		y=*head;
-		while(y->next!=NULL)
-		{
-			y=y->next;
-		}
-		y->next=p;
-		p->next=NULL;
-	}
-}
-void display(struct node* head)
-{
-	struct node *text=head;
-	while(text!=NULL)
-	{
-		cout<<text->data;
-		text=text->next;
-	}
-	cout<<endl;
-}                                  
-void pairwiseswap(struct node* head)
-{                                              //pairwise swap a linked list.
-	struct node* temp=head;
-	struct node* temp1=temp->next;
-	while(temp!=NULL&&temp1->next!=NULL)
-	{
-		swap(temp->data,temp1->data);
-		temp=temp->next->next;
-		temp1=temp->next;
-	}
-	swap(temp->data,temp1->data);
-}
-void swap(int a,int b)
-{
-	int c;
-	c=a;
-	a=b;
-	b=c;
-}
-int main()
-{
-	int n,number,number1,d;               
-	struct node *head=NULL;
-	struct node *head1=NULL;
-	cout<<"enter number of elements in linked list 1.";
-	cin>>n;
-	cout<<"first linked list"<<endl;
-	for(int i=n;i>0;i--)
-	{
-		cin>>number;
-		insert(&head,number);
-	}
-	pairwiseswap(head);
-	display(head);
-return 0; 
-}
+
+    Node* new_node = new Node(); 
+    new_node->data = new_data; 
+    new_node->next = (*head_ref); 
+    (*head_ref) = new_node; 
+} 
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data << " "; 
+        node = node->next; 
+    } 
+} 
+int main() 
+{ 
+    Node* start = NULL; 
+    push(&start, 5); 
+    push(&start, 4); 
+    push(&start, 3); 
+    push(&start, 2); 
+    push(&start, 1); 
+ //   push(&start,6);
+   cout << "Linked list "
+         << "before calling pairWiseSwap()\n"; 
+    printList(start); 
+  
+    pairWiseSwap(start); 
+  
+    cout << "\nLinked list "
+         << "after calling pairWiseSwap()\n"; 
+    printList(start); 
+  
+    return 0; 
+} 
+  
